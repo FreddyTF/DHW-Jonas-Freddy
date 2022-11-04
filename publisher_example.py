4361085262e0663e3cf405da812ce749614e95a6
Frederic Iron <50624465+FreddyTF@users.noreply.github.com>
2022-11-04 14:54:07 +0100

    fixed time sleep in publisher

--- publisher_example.py
+++ publisher_example.py
@@ -16,17 +16,13 @@ client.connect(broker_adress)
 json_dict = {"fin":"WDDU411STM9032150", "zeit":"01.01.2020 15:34:05.123", "geschwindigkeit":200, "ort":1}
 
 ### Publish forever ###
-# publish at nearly exact 5 s
-target_time = time.perf_counter_ns()
 while True:
-    target_time += 5 * 10**9
     # change the time to the current time
     json_dict["zeit"] = str(datetime.datetime.now())
     json_dict["geschwindigkeit"] = np.random.randint(0, high=200)
     # publish the dict
     client.publish("DataMgmt/FIN",json.dumps(json_dict), qos=1)
     print(f"Published: {json.dumps(json_dict)}")
-
-    # wait to get nearly exakt 5 s delay
+    time.sleep(5)
 
 
