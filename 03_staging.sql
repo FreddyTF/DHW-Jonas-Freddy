8590b3049767f7e3b6e6e572d038c9612dd6f6a4
Frederic Iron <50624465+FreddyTF@users.noreply.github.com>
2022-11-04 13:51:35 +0100

    add dataset

--- 03_staging.sql
+++ 03_staging.sql
@@ -1,6 +1,8 @@
 insert into staging.kunde (kunde_id, vorname, nachname, anrede, geschlecht, geburtsdatum, wohnort, quelle)
-values (171893, ‚Kevin', Minion', 'Herr', 'männlich', to_date('12.02.1990', 'DD.MM.YYYY'), 7, 'CRM');
+values (171894, 'Max', 'Musterman', 'Herr', 'männlich', to_date('12.02.1990', 'DD.MM.YYYY'), 7, 'CRM');
+		
 insert into staging.fahrzeug (fin, kunde_id, baujahr, modell, quelle)
-values ('SNTU411STM9032150', 171893, 1985, 'Trabant 601 de luxe', 'Fahrzeug DB');
+values ('WDDU411STM9032150', 171893, 1985, 'Mercedes', 'Fahrzeug DB');
+
 insert into staging.kfzzuordnung (fin, kfz_kennzeichen, quelle)
-values ('SNTU411STM9032150', 'UL-DV 111', 'Fahrzeug DB');
+values ('WDDU411STM9032150', 'FR-FR 701', 'Fahrzeug DB');
